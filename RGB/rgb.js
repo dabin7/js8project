5f2d7338c632588dc65f6c5bc266922623dd766e
dabin7 <107933140+dabin7@users.noreply.github.com>
2022-10-11 15:43:01 +0900

    Update

--- RGB/rgb.js
+++ RGB/rgb.js
@@ -1,8 +1,8 @@
 const randomRgb = document.getElementById('randomRgb');
 const colorBox = document.querySelectorAll('.colorBox');
 const reset = document.querySelector('.reset');
+const colorBoxs = document.getElementsByClassName('.color');
 
-let numSquares = 9;
 let colors = [];
 let pickedColor;
 
@@ -11,7 +11,7 @@ function makeColor() {
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
 
-  return 'rgb(' + r + ',' + g + ',' + b + ')';
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 
 function colorPick() {
@@ -22,12 +22,25 @@ function colorPick() {
   randomRgb.innerHTML = pickedColor;
 }
 
+function clickColor(event) {
+  const clickRgb = event.target.style.backgroundColor;
+  if (clickRgb === pickedColor) {
+    for (let i = 0; i < colorBox.length; i++) {
+      colorBox[i].style.backgroundColor = pickedColor;
+    }
+    randomRgb.style.backgroundColor = pickedColor;
+  }
+  //clickRgb === pickedColor ?? 완전히 같아야됨. 띄어쓰기까지
+}
+
 function resetColor() {
   colors = [];
   colorPick();
+  randomRgb.style.backgroundColor = '';
   // querySelectorAll 은 for문을 통해서만 변환
 }
 
 reset.addEventListener('click', resetColor);
+window.addEventListener('click', clickColor);
 
 colorPick();
