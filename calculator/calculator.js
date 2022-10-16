8041e703220a60e4d23ba6ae3dbc6110ca084067
dabin7 <brian421@naver.com>
2022-10-16 13:50:43 +0900

    Update

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,8 +4,26 @@ const equal = document.querySelector('.result');
 const clear = document.querySelector('.clear');
 const operator = document.querySelectorAll('.operator');
 
-function numberBtn() {
-  inputBox.value = 1;
+function numberBtn(e) {
+  if (inputBox.value[0] === '0') {
+    inputBox.value = '';
+    inputBox.value += e.target.value;
+  } else {
+    inputBox.value += e.target.value;
+  }
+}
+
+function operatorBtn(e) {
+  let result = 0;
+  if (e.target.value === '+') {
+    result = +inputBox.value + 
+  }
+  if (e.target.value === '-') {
+  }
+  if (e.target.value === 'x') {
+  }
+  if (e.target.value === '/') {
+  }
 }
 
 function resultBtn() {}
@@ -14,6 +32,9 @@ function clearBtn() {}
 
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener('click', numberBtn);
+}
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener('click', operatorBtn);
 }
 equal.addEventListener('click', resultBtn);
 clear.addEventListener('click', clearBtn);
